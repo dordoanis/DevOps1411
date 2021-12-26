776e618e2d4f9025fb1e1b4b7676f96ca3f62150
Dor <dordo@checkpoint.com>
2021-12-26 18:34:21 +0200

    add 1.py

---
+++ 1.py
@@ -0,0 +1,29 @@
+print('hello')
+my_name = "Dor"
+print(my_name)
+
+is_true = False
+# my comment about this variable
+my_list = ["Dor", "Aviel", 31, True]
+print(my_list[1])
+my_dict = {"name": "dor",
+           "lname": "hoana",
+            "age": 27,
+            "is married":True}
+print((my_dict["is married"]))
+print(my_dict.keys())
+
+my_numer = 5 * 2
+my_other = 5 * "Dor"
+print(my_other)
+
+if my_numer == 10:
+    print("my number is 10 ")
+
+fname = "Dor"
+lname = "Doanis"
+print("hello" + fname + " " + lname)
+print(f"hello {fname} {lname}")
+print("hello %s %s" % (fname, lname))
+
+
