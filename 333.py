3db679f9c566a9253566182df82fd80a5582605f
Dor Doanis <92949518+superlow883@users.noreply.github.com>
2021-12-26 19:19:28 +0200

    Create 333.py (#1)
    * Create 333.py
    
    * Create 444.py

---
+++ 333.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from time import sleep
+import requests
+
+response = requests.get("https://github.com")
+if response.status_code == 200:
+    print("github is OK")
+print(datetime.now())
+sleep(10)
+print(datetime.now())
--- 444.py
+++ 444.py
@@ -1,1 +1,33 @@
-print("conflict")
+
+# import requests
+# try:
+#     response = requests.get("https://blablabla.com")
+# except requests.exceptions.ConnectionError:
+#     print("Unable to connect to host ")
+
+# try:
+#     a = int(input("enter first num: "))
+#     b = int(input("enter first num: "))
+#     result = a / b
+#     print(result)
+# except ZeroDivisionError:
+#     print("could not divide by Zero")
+# except BaseException as e:
+#     print("somthing went worng " + str(e.args))
+
+
+def get_user_age():
+    age = int(input("enter your age: "))
+    if age < 0:
+        raise ValueError("Age can not be negativ")
+    return age
+
+age = -1
+while age < 0:
+
+    try:
+        get_user_age()
+    except ValueError as e:
+        print(e.args[0])
+
+print(f"your age: {age}")
