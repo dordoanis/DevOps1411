66af4787ff14b2efa0b88c2efcec9dcf72b077a0
Dor <dordo@checkpoint.com>
2021-12-26 18:39:23 +0200

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,54 @@
+what_to_print = "Hello Word"
+# print(what_to_print)
+# print(what_to_print)
+# print(what_to_print)
+# print(what_to_print)
+#
+# print(what_to_print * 4)
+# print(list(range(9, 2, -1)))
+# Start Stop Step
+
+amount_of_prints = 5
+list_of_names = ["hen", "michael", "noam", "lior", "amichai"]
+for i in range(1, amount_of_prints):
+    print(str(i) + ") " + str(list_of_names))
+
+# for i in range(len(list_of_names)):
+#     print(list_of_names[i])
+# #
+# for name in list_of_names:
+#     if name == "hen":
+#         continue
+#     print(name)
+
+# a = 0
+# while a < 10:
+#     print(a)
+#     a = a + 1
+#
+# 7 Boom
+# for i in range(1, 101):
+#     if i % 7 == 0 or "7" in str(i):
+#         continue
+#     print(i)
+#
+# for i in range(1, 101):
+#     if i % 7 == 0 or "7" in str(i):
+#         pass
+#     else:
+#         print("hey")
+#     print(i)
+#
+#
+# to know if while end
+# a = "x"
+# while a != "q":
+#     a = input("enter q or not: ")
+#
+# else:
+#     print("finished")
+#######################
+# for i in range(5):
+#     print(i)
+# else:
+#     print("finished")
